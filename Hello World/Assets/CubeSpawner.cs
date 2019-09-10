9efd388e1754826611e921b7bf81844e9b7cc4be
grim_reality <connorstark@hotmail.com>
2019-09-10 14:16:00 -0400

    Second Commit

---
+++ Hello World/Assets/CubeSpawner.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeSpawner : MonoBehaviour
+{
+
+
+    public GameObject cubeprefab;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Instantiate(cubeprefab);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Instantiate(cubeprefab);
+    }
+}
